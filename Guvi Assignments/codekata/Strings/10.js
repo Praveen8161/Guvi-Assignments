614e4f6224d1a22a9a04d48149bb98d05b4e6aac
Praveen8161 <rbalu8161@gmail.com>
2023-08-29 16:30:12 +0530

    strings 10th question chnages

--- Guvi Assignments/codekata/Strings/10.js
+++ Guvi Assignments/codekata/Strings/10.js
@@ -1,28 +1,22 @@
-let s = "I am john cena cena john monday".split(" ");
-console.log(s);
-let result = [];
-let case1 = [];
-let case2 = [];
-function dupRemover(case1,case2) {
-   for (var j = 0;j<case1.length; j++){
-         if (case2.includes(case1[j])){
-            if(case1[j] === case2[j]){
-               continue;
-            }
-         }else{
-            result.push(case1[j]);
-         }
+let s = "We love we love".split(" ");
+
+function remAdjWords(arr){
+   let uniWords  = arr.slice(0,arr.length);
+   let start;
+   let len = 2;
+   
+   for (var i = 0; i<arr.length; i++){
+      if(arr[i] === arr[i+1]){
+         start = i;
+         arr.splice(start,len);
+         break;
+      }
    }
-}
-for (var i=0; i<s.length; i++){
-   if (s[i] !== s[i+1]){
-      result.push(s[i]);
-   }else if (s[i] === s[i+1]){
-      result = [];
-      case1 = case1.concat(s.slice(0,i+1));
-      case2 = case2.concat(s.slice(i+1)).reverse();
-      dupRemover(case1,case2);
-      break;
+   if(uniWords.length === arr.length){
+      return arr
    }
+      return remAdjWords(arr);
 }
-console.log(result.join(" "));
+
+let result  = remAdjWords(s).join(" ");
+console.log(result);
--- Guvi Assignments/script.js
+++ Guvi Assignments/script.js
@@ -5,9 +5,22 @@ let arrvar = [30,30.5,"selvi",false,50,"45"];
 let objvar = {"name":"Praveen","age":26,"city":"salem"};
 console.log(objvar) */
 
-const a = {he:1,she:2,
-        b:{bhe:"12",bshe:"45"}
-    };
-console.log(a);
-console.log(a.b);
-console.log(a.b.bhe);
+let s = "I am john cena cena john".split(" ");
+
+function remAdjWords(arr){
+   let uniWords = arr;
+   let start;
+   let len = 1;
+   for (var i in uniWords){
+      if(uniWords[i] === uniWords[i+1]){
+         start = i;
+         uniWords.splice(start,len);
+         break;
+      }else{
+         return uniWords;
+      }
+   }
+      return remAdjWords(uniWords);
+}
+let result  = remAdjWords(s);
+console.log(result);
